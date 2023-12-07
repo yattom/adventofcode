41b86faf65feb45b4069826f9782797466426eaf
yattom <tsutomu.yasui@gmail.com>
2023-12-07 09:20:05 +0900

    refactor 2023_06

--- 2023_06/puzzle_test.py
+++ 2023_06/puzzle_test.py
@@ -1,3 +1,5 @@
+import math
+
 def test_solve():
     puzzle_input = [
         "Time:      7  15   30",
@@ -13,16 +15,60 @@ def test_solve2():
     ]
     assert solve2(puzzle_input) == 71503
 
-
 def count_winnable_ways(total_time, best_distance):
-    tally = 0
-    for push_time in range(0, total_time + 1):
+    p1 = (total_time + math.sqrt(total_time ** 2 - 4 * best_distance)) / 2
+    p2 = (total_time - math.sqrt(total_time ** 2 - 4 * best_distance)) / 2
+    return abs(math.ceil(min([p1, p2]) + 0.000001) - math.floor(max([p1, p2]) - 0.00001)) + 1
+
+
+def count_winnable_ways_binary_search(total_time, best_distance):
+    def is_win(push_time):
         v = push_time
         run_time = total_time - push_time
         run_distance = v * run_time
-        if run_distance > best_distance:
-            tally += 1
-    return tally
+        return run_distance > best_distance
+
+    print('@1')
+    push_time = 0
+    width = total_time // 2
+    to_win = [-1, -1]
+    while True:
+        print(f'{push_time=} {width=}')
+        print(f'{is_win(push_time)=} {is_win(push_time + 1)=}')
+        if not is_win(push_time) and is_win(push_time + 1):
+            to_win[0] = push_time + 1
+            break
+
+        width = width // 2
+        if width == 0:
+            width = 1
+        if is_win(push_time):
+            push_time -= width
+        else:
+            push_time += width
+        if push_time < 0:
+            raise RuntimeError()
+
+    push_time = total_time
+    width = total_time // 2
+    print('@2')
+    while True:
+        print(f'{push_time=} {width=}')
+        print(f'{is_win(push_time)=} {is_win(push_time + 1)=}')
+        if is_win(push_time) and not is_win(push_time + 1):
+            to_win[1] = push_time
+            break
+
+        width = width // 2
+        if width == 0:
+            width = 1
+        if is_win(push_time):
+            push_time += width
+        else:
+            push_time -= width
+        if push_time < 0:
+            raise RuntimeError()
+    return to_win[1] - to_win[0] + 1
 
 
 def solve(puzzle_input):
